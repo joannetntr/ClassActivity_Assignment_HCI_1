2cbbd7b24c460a980852e74f8bd002593a1e5fe8
joannetntr <101113276+joannetntr@users.noreply.github.com>
2022-12-21 09:07:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //Please put your answer here
+$(function() {
+  $('.signup-show').click(function(){
+    $('#signup-modal').fadeIn();
+  });
+
+});
+
+$(function() {
+  $('#close-modal').click(function(){
+    $('#signup-modal').fadeOut();
+  });
+
+});
